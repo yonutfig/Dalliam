998c846067607612726d570b9674ad7fd1f0d538
yonutfig <yonut.fig@yahoo.com>
2024-10-15 21:27:40 +0100

    Phone navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,29 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../Assets/Images/Capture-removebg-preview.png";
 import "./Navbar.css";
+
 const Navbar = () => {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <div className="home-container">
       <nav className="navbar">
         <div className="navbar-logo">
           <img src={logo} alt="Company Logo" className="logo-image" />
         </div>
-        <ul className="navbar-links">
+        <div className="menu-icon" onClick={toggleMenu}>
+          &#9776; {/* Hamburger Icon */}
+        </div>
+        <ul className={`navbar-links ${menuActive ? "active" : ""}`}>
+          <div className="close-icon" onClick={toggleMenu}>
+            &times; {/* Close Icon */}
+          </div>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={toggleMenu}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/aircon">Aircon</Link>
+            <Link to="/aircon" onClick={toggleMenu}>
+              Aircon
+            </Link>
           </li>
           <li>
-            <Link to="/heat-pumps">Heat Pumps</Link>
+            <Link to="/heat-pumps" onClick={toggleMenu}>
+              Heat Pumps
+            </Link>
           </li>
           <li>
-            <Link to="/service">Services</Link>
+            <Link to="/service" onClick={toggleMenu}>
+              Services
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact" onClick={toggleMenu}>
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
