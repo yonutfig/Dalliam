f66f579ab5aea8a13757064b0217ebef1b73f978
yonutfig <yonut.fig@yahoo.com>
2024-10-20 22:48:41 +0100

    update

--- src/Components/Caroussel/VideoCarousel.js
+++ src/Components/Caroussel/VideoCarousel.js
@@ -51,7 +51,7 @@ const VideoCarousel = () => {
           initial={{ opacity: 0, scale: 1.2 }}
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0, scale: 0.8 }}
-          transition={{ duration: 1.5, ease: "easeInOut" }}
+          transition={{ duration: 1.5, ease: "easeOut" }}
           onCanPlayThrough={handleVideoLoad}
         >
           Your browser does not support the video tag.
